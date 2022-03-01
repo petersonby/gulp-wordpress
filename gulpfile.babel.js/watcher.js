1ab30336e6ebcb5e261fe328b0ed4836f13ac306
Alexey Peterson <contact@peterson.by>
2022-03-01 15:51:23 +0300

    fix: add lint task to watcher

--- gulpfile.babel.js/watcher.js
+++ gulpfile.babel.js/watcher.js
@@ -1,11 +1,12 @@
 import { watch, series } from 'gulp';
 import { styles } from './styles';
 import { scripts } from './scripts';
+import { lint } from './lint';
 import { svgSprites } from './svgSprites';
 
 export function watcher(done) {
 	watch('src/scss/**/*.scss', { ignoreInitial: false }, series(styles));
-	watch('src/js/**/*.js', { ignoreInitial: false }, series(scripts));
+	watch('src/js/**/*.js', { ignoreInitial: false }, series(scripts, lint));
 	watch('assets/img/svg/*.svg', { ignoreInitial: false }, series(svgSprites));
 	done();
 }
